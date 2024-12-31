62b4b123df0498293ec08ea598b1bc91a542171c
Karel Van Hecke <25814162+karelvanhecke@users.noreply.github.com>
2024-12-31 14:10:30 +0100

    ci(build): make builds truly reproducible (#142)

--- internal/version/version.go
+++ internal/version/version.go
@@ -17,18 +17,87 @@ limitations under the License.
 package version
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"runtime"
+	"runtime/debug"
+
+	"gopkg.in/yaml.v3"
 )
 
 var (
 	version = ""
-	commit  = ""
 )
 
-func Info() {
-	fmt.Printf("version: %s\ncommit: %s\nplatform: %s\n",
-		version,
-		commit,
-		runtime.GOOS+"/"+runtime.GOARCH)
+type Info struct {
+	Version       string `yaml:"version" json:"version"`
+	GitCommit     string `yaml:"gitCommit" json:"gitCommit"`
+	GitCommitDate string `yaml:"gitCommitDate" json:"gitCommitDate"`
+	GitTreeState  string `yaml:"gitTreeState" json:"gitTreeState"`
+	GoVersion     string `yaml:"goVersion" json:"goVersion"`
+	Compiler      string `yaml:"compiler" json:"compiler"`
+	Platform      string `yaml:"platform" json:"platform"`
+}
+
+func NewInfo() *Info {
+	i := &Info{}
+
+	var os, arch string
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return i
+	}
+
+	for _, v := range bi.Settings {
+		switch v.Key {
+		case "vcs.revision":
+			i.GitCommit = v.Value
+		case "vcs.time":
+			i.GitCommitDate = v.Value
+		case "vcs.modified":
+			if v.Value == "true" {
+				i.GitTreeState = "dirty"
+			} else {
+				i.GitTreeState = "clean"
+			}
+		case "GOOS":
+			os = v.Value
+		case "GOARCH":
+			arch = v.Value
+		case "-compiler":
+			i.Compiler = v.Value
+		}
+	}
+	if version == "" {
+		i.Version = "sha-" + i.GitCommit
+	} else {
+		i.Version = version
+	}
+	i.GoVersion = bi.GoVersion
+	i.Platform = os + "/" + arch
+
+	return i
+}
+
+func (i *Info) String() string {
+	return fmt.Sprintf("version: %s", i.Version)
+}
+
+func (i *Info) JSON() (string, error) {
+	s, err := json.MarshalIndent(i, "", "")
+	if err != nil {
+		return "", err
+	}
+	return string(s), nil
+}
+
+func (i *Info) YAML() (string, error) {
+	buf := bytes.NewBuffer([]byte{})
+	enc := yaml.NewEncoder(buf)
+	enc.SetIndent(2)
+	err := enc.Encode(i)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
